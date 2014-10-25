c2a0e2455f412d9a6a28504feac48ae44385f3c9
Binh Nguyen <ntbinhptnk@gmail.com>
2014-10-25 23:28:45 +0700

    Initial commit

---
+++ Main codes/data-smashing-api.py
@@ -0,0 +1,10 @@
+import traceback
+import sys
+import bson
+from datetime import *
+import os
+
+
+if __name__ == "__main__":
+    list_arguments=ast.literal_eval(str(sys.argv))
+    print list_arguments
