9c799b4586ef859474838d62db92ec335ce15f90
Binh Nguyen <ntbinhptnk@gmail.com>
2014-11-08 22:10:45 +0700

    Add basic format for data-smashing class

--- Main codes/data-smashing-api.py
+++ Main codes/data-smashing-api.py
@@ -5,6 +5,18 @@ from datetime import *
 import os
 
 
+class data_smashing_api:
+    
+    def independent_streaming_copy(s):
+    
+    def stream_inversion(s):
+    
+    def stream_summation(s1,s2):
+    
+    def deviation(s):
+    
+
+
 if __name__ == "__main__":
     list_arguments=ast.literal_eval(str(sys.argv))
     print list_arguments
